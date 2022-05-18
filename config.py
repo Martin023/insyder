501722630e48334956d606f1e2f904377dce76df
martin023 <mainamartin023@gmail.com>
2022-05-18 17:44:47 +0300

    Update database

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin023:0000@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin023:0000@localhost/insyder'
     SECRET_KEY = 'werkzeug'
     QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin023:0000@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin023:0000@localhost/insyder'
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",)
     
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin023:0000@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin023:0000@localhost/insyder'
     
     DEBUG = True
     
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 SECRET_KEY = 'werkzueg'
-DATABASE_URL = 'postgresql+psycopg2://martin023:0000@localhost/blog'
+DATABASE_URL = 'postgresql+psycopg2://martin023:0000@localhost/insyder'
 
 python3.8 manage.py server
