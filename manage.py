c59da167c90f5cff93a6c66fcb39838ccd276960
martin023 <mainamartin023@gmail.com>
2022-05-18 17:36:51 +0300

    This commit will update manage.py

--- manage.py
+++ manage.py
@@ -1,17 +1,28 @@
-from app import create_app
+from app import create_app, db
+from app.models import User,Blog,Comment,Quote
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 
-app=create_app('development')
+# app = create_app('test')
+# app = create_app('development')
+app = create_app('production')
+migrate = Migrate(app, db)
 
-manager=Manager(app)
+manager =  Manager(app)
+manager.add_command('db',MigrateCommand)
 manager.add_command('server',Server)
 
-# @manager.command
-# def test():
-#     #function to run unittests
-#     import unittest
-#     tests=unittest.TestLoader().discover('tests')
-#     unittest.TextTestResult(verbosity=2).run(tests)
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Blog = Blog, Comment = Comment, Quote = Quote )
 
-if __name__=='__main__':
+manager.add_command('server', Server)
+@manager.command
+def test():
+    import unittest
+    test = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(test)
+
+
+if __name__ == '__main__':
     manager.run()
